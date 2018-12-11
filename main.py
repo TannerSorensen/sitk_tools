174e79f7b51de5608261858df813a3d866920349
Tanner Sorensen <tsorense@usc.edu>
2018-12-11 14:16:27 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+# coding: utf-8
+
+import SimpleITK as sitk
+import numpy as np
+import os
+
+def get_superresolution_recon(img, ref_img):
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetDefaultPixelValue(0.0)
+
+    # sagittal image is the reference
+    resampler.SetReferenceImage(ref_img)
+
+    # 5th order b-spline interpolator
+    resampler.SetInterpolator(sitk.sitkBSpline)
+
+    # rotate image to reference orientation
+    resampler.SetOutputDirection(ref_img.GetDirection())
+
+    # origin
+    resampler.SetOutputOrigin(ref_img.GetOrigin())
+
+    # output pixel type
+    resampler.SetOutputPixelType(sitk.sitkUInt16)
+
+    # output image spacing (isotropic at in-plane resolution)
+    in_plane_resolution = min(ref_img.GetSpacing())
+    resampler.SetOutputSpacing((in_plane_resolution,in_plane_resolution,in_plane_resolution))
+
+    # set output size
+    siz = ref_img.GetSize()
+    spc = ref_img.GetSpacing()
+    resampler.SetSize((int(siz[0]*spc[0]/in_plane_resolution), 
+                       int(siz[1]*spc[1]/in_plane_resolution), 
+                       int(round(siz[2]*spc[2]/in_plane_resolution))))
+
+    # identity transform (no transform necessary for interpolation step)
+    resampler.SetTransform(sitk.Transform(ref_img.GetDimension(), sitk.sitkIdentity))
+
+    img_resampled = resampler.Execute(img)
+
+    return img_resampled
+
+def write_stats_image(arr, spacing, direction):
+    stats_img = sitk.GetImageFromArray(arr)
+    stats_img.SetSpacing(spacing)
+    stats_img.SetDirection(direction)
+    sitk.WriteImage(stats_img,"resampled_mean.mha")
+
+# get paths to directories containing the three orthogonal DICOM MR image stacks
+t2wi_path = os.path.join(os.getcwd(),"data","js")
+img_stack_folders = os.listdir(t2wi_path)
+sag_dir = [x for x in img_stack_folders if "sag" in x.lower()][0]
+cor_dir = [ x for x in img_stack_folders if "cor" in x.lower()][0]
+axial_dir = [ x for x in img_stack_folders if "axial" in x.lower()][0]
+
+# Sagittal image (reference)
+print("resample sagittal image stack")
+reader = sitk.ImageSeriesReader()
+dicom_names = reader.GetGDCMSeriesFileNames( os.path.join(t2wi_path,sag_dir))
+reader.SetFileNames(dicom_names)
+sag_img = reader.Execute()
+sag_img_resampled = get_superresolution_recon(sag_img, sag_img)
+sitk.WriteImage(sag_img_resampled, "resampled_sag.mha")
+
+# Coronal image
+print("resample coronal image stack")
+reader = sitk.ImageSeriesReader()
+dicom_names = reader.GetGDCMSeriesFileNames(os.path.join(t2wi_path,cor_dir))
+reader.SetFileNames(dicom_names)
+cor_img = reader.Execute()
+cor_img_resampled = get_superresolution_recon(cor_img, sag_img_resampled)
+sitk.WriteImage(cor_img_resampled, "resampled_cor.mha")
+
+# Resample axial image
+print("resample axial image stack")
+reader = sitk.ImageSeriesReader()
+dicom_names = reader.GetGDCMSeriesFileNames(os.path.join(t2wi_path,axial_dir))
+reader.SetFileNames(dicom_names)
+axial_img = reader.Execute()
+axial_img_resampled = get_superresolution_recon(axial_img, sag_img_resampled)
+sitk.WriteImage(axial_img_resampled, "resampled_axial.mha")
+
+# Fuse the three MR image stacks using the mean, median, max, and min of the three images 
+print("fusing images")
+arr = np.stack([sitk.GetArrayFromImage(sag_img_resampled),
+                sitk.GetArrayFromImage(cor_img_resampled),
+                sitk.GetArrayFromImage(axial_img_resampled)])
+write_stats_image(np.mean(arr, axis=0), sag_img_resampled.GetSpacing(), sag_img_resampled.GetDirection())
+write_stats_image(np.median(arr, axis=0), sag_img_resampled.GetSpacing(), sag_img_resampled.GetDirection())
+write_stats_image(np.max(arr, axis=0), sag_img_resampled.GetSpacing(), sag_img_resampled.GetDirection())
+write_stats_image(np.min(arr, axis=0), sag_img_resampled.GetSpacing(), sag_img_resampled.GetDirection())
+
---
+++ to_dcm.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import SimpleITK as sitk
+
+img_filename = sys.argv[1]
+output_path = sys.argv[2]
+
+img = sitk.ReadImage(img_filename)
+
+def writeSlices(series_tag_values, img, i):
+    castFilter = sitk.CastImageFilter()
+    castFilter.SetOutputPixelType(sitk.sitkInt16)
+    
+    # Convert floating type image (imgSmooth) to int type (imgFiltered)
+    image_slice = castFilter.Execute(img[:,:,i])
+
+    # Tags shared by the series.
+    list(map(lambda tag_value: image_slice.SetMetaData(tag_value[0], tag_value[1]), series_tag_values))
+
+    # Slice specific tags.
+    image_slice.SetMetaData("0008|0012", time.strftime("%Y%m%d")) # Instance Creation Date
+    image_slice.SetMetaData("0008|0013", time.strftime("%H%M%S")) # Instance Creation Time
+
+    # Setting the type to CT preserves the slice location.
+    image_slice.SetMetaData("0008|0060", "CT")  # set the type to CT so the thickness is carried over
+
+    # (0020, 0032) image position patient determines the 3D spacing between slices.
+    image_slice.SetMetaData("0020|0032", '\\'.join(map(str,img.TransformIndexToPhysicalPoint((0,0,i))))) # Image Position (Patient)
+    image_slice.SetMetaData("0020,0013", str(i)) # Instance Number
+
+    # Write to the output directory and add the extension dcm, to force writing in DICOM format.
+    writer.SetFileName(os.path.join(output_path,str(i).zfill(4)+'.dcm'))
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    writer.Execute(image_slice)
+
+modification_time = time.strftime("%H%M%S")
+modification_date = time.strftime("%Y%m%d")
+
+# Copy some of the tags and add the relevant tags indicating the change.
+# For the series instance UID (0020|000e), each of the components is a number, cannot start
+# with zero, and separated by a '.' We create a unique series ID using the date and time.
+# tags of interest:
+direction = img.GetDirection()
+series_tag_values = [("0008|0031",modification_time), # Series Time
+                  ("0008|0021",modification_date), # Series Date
+                  ("0008|0008","DERIVED\\SECONDARY"), # Image Type
+                  ("0020|000e", "1.2.826.0.1.3680043.2.1125."+modification_date+".1"+modification_time), # Series Instance UID
+                  ("0020|0037", '\\'.join(map(str, (direction[0], direction[3], direction[6],# Image Orientation (Patient)
+                                                    direction[1],direction[4],direction[7])))),
+                  ("0008|103e", "Created-SimpleITK")] # Series Description
+
+
+writer = sitk.ImageFileWriter()
+# Use the study/series/frame of reference information given in the meta-data
+# dictionary and not the automatically generated information from the file IO
+writer.KeepOriginalImageUIDOn()
+
+list(map(lambda i: writeSlices(series_tag_values, img, i), range(img.GetDepth())))
